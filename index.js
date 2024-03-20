318681e8239ca023f325df51b396dbd010228f6a
brianchege2k <chegebrian84@gmail.com>
2024-03-20 09:27:26 +0300

    strings lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Njuguna Chege'
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+console.log(welcomeMessage)
+
+const excitedWelcomeMessage =  `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+console.log(excitedWelcomeMessage)
+const shortGreeting = `Welcome,  ${currentUser.slice(0,1)}!`
